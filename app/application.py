5e4e67e5ac0f176f8f0e7a5d3de2ba06b224641e
p.dunyoh <p.dunyoh@fintechandpaymentsinstitute.com>
2024-04-17 14:16:19 +0000

    set up mongo client for db

--- app/application.py
+++ app/application.py
@@ -1,5 +1,23 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
+from app.controllers import app
+from flask_pymongo import PyMongo
 
-# from app.controllers import api
+app.config['SECRET_KEY'] = '689727683effe5980db7ec9af471311b2ebf9c7f'
+app.config[
+    'MONGO_URI'] = "mongodb+srv://priscilladunyoh99:Q7OXtTY2z66B1vj3@cluster0.gsxghkt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongo_client = PyMongo(app)
+db = mongo_client.db
+
+from pymongo.mongo_client import MongoClient
+#
+# uri = "mongodb+srv://priscilladunyoh99:Q7OXtTY2z66B1vj3@cluster0.gsxghkt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+#
+# # Create a new client and connect to the server
+# client = MongoClient(uri)
+#
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
